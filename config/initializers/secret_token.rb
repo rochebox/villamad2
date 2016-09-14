507304549d3ede226cf4ebb2c5f6f09f4a1c9051
rochebox <rochebox@gmail.com>
2016-09-14 21:17:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Villamad2::Application.config.secret_key_base = '1efc3c76eed2e1d068ff8e94c77983a7e027554c0c70e75c405feae6d268298dc0b4022955a9c460d6c66e70f9ccd3983742c63032dad76b9d6210608b7cd4d3'
